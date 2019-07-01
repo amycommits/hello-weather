c48685972cdf83dbb7ce29a331ac8a03298ffdea
duaimei <adetwiler@optoro.com>
2019-07-01 13:36:54 -0400

    Added the forecast data, and prepared the html to add the weather

--- App.vue
+++ App.vue
@@ -1,8 +1,10 @@
 <template>
-  <view class="container">
-    <text>{{ location }}</text>
-    <text>{{ now['weather'] }}</text>
-    <text>{{ now['temp'] }}</text>
+  <view class="app">
+    <view class="now-container">
+      <text>{{ location }}</text>
+      <text>{{ now['weather'] }}</text>
+      <text>{{ now['temp'] }}</text>
+    </view>
   </view>
 </template>
  <script>
@@ -14,6 +16,33 @@
        now: {
          temp: 97,
          weather: 'cloudy'
+       },
+       forecast: {
+         '2019-07-25': {
+           'temp-high': 96,
+           'temp-low': 85,
+           'weather': 'rainy'
+         },
+         '2019-07-26': {
+           'temp-high': 98,
+           'temp-low': 88,
+           'weather': 'rainy'
+         },
+         '2019-07-27': {
+           'temp-high': 92,
+           'temp-low': 72,
+           'weather': 'sunny'
+         },
+         '2019-07-28': {
+           'temp-high': 87,
+           'temp-low': 77,
+           'weather': 'cloudy'
+         },
+         '2019-07-29': {
+           'temp-high': 95,
+           'temp-low': 83,
+           'weather': 'cloudy'
+         }
        }
      }
    }
